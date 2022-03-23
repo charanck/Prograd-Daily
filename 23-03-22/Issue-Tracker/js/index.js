ce0c1934965f7611e9aeb6af898cf43cb68e2bd6
charanck <charank804.com>
2022-03-23 09:42:12 +0530

    Initial commit

---
+++ 23-03-22/Issue-Tracker/js/index.js
@@ -0,0 +1,22 @@
+// Initial Setup
+User.loadToMemory();
+Issue.loadToMemory();
+Comment.loadToMemory();
+
+// const charan = new User(null,"Charan","Charan@123");
+// const issue1 = new Issue(null,"Some random description","High","backlog",charan,charan,null);
+// const comment1 = new Comment(null,"New COmment@!!",charan,issue1,null);
+// console.log(issue1);
+
+
+
+// const charan = User.getAllUsers()[0];
+// const issue1 = Issue.getAllIssues()[0];
+// console.log(Comment);
+
+// const comment2 = new Comment("SHIT",charan,issue1);
+
+// Comment.delete(Comment.getAllComments()[0]);
+
+// id, comment, commentedBy, commentTo, commentedOn
+
