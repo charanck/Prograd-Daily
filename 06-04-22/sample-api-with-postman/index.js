b094111048bea6b34d8c6b309fef4e3577702342
charanck <charank804@gmail.com>
2022-04-06 14:54:14 +0530

    Fixed sample api with postman

--- 06-04-22/sample-api-with-postman/index.js
+++ 06-04-22/sample-api-with-postman/index.js
@@ -4,20 +4,25 @@ const app = express();
 
 let users = [];
 
-app.post('user',(req,res)=>{
+app.post('/user',(req,res)=>{
     let user = req.body;
     user.id = users.length + 1;
     users.push(user);
     res.json(user);
 });
 
-app.get('user',(req,res)=>{
+app.get('/user',(req,res)=>{
     res.json(users[users.length - 1]);
 });
 
-app.delete('user',(req,res)=>{
+app.delete('/user',(req,res)=>{
     const user = users.pop();
     res.json(user);
+})
+
+app.all('/*',(req,res)=>{
+    res.status(404);
+    res.json({'message':'Page not found'});
 })
 
 app.listen(3000,()=> console.log('Listening on 3000...'));
