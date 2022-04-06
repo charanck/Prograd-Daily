20f2ee2d4b741b1001656205120c15dc970024f6
charanck <charank804@gmail.com>
2022-04-06 12:21:01 +0530

    Competed sample api with postman connection

---
+++ 06-04-22/sample-api-with-postman/index.js
@@ -0,0 +1,23 @@
+const express = requires('express');
+
+const app = express();
+
+let users = [];
+
+app.post('user',(req,res)=>{
+    let user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.json(user);
+});
+
+app.get('user',(req,res)=>{
+    res.json(users[users.length - 1]);
+});
+
+app.delete('user',(req,res)=>{
+    const user = users.pop();
+    res.json(user);
+})
+
+app.listen(3000,()=> console.log('Listening on 3000...'));
