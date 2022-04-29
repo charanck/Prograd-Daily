1fb8c4d36c3211bef59755fe4c4f52c9c98b90c4
charanck <charank804@gmail.com>
2022-04-29 14:33:17 +0530

    completed webapp with 4 components task

---
+++ 29-04-22/webapp-with-4-components/sample-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { CardComponent } from './card/card.component';
+import { AlertComponent } from './alert/alert.component';
+import { InputComponent } from './input/input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    CardComponent,
+    AlertComponent,
+    InputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
